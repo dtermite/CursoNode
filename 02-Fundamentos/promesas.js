ecd73d3421930d9b27292ba63f420f08d04853f1
Diego Termite <diegotermite@gmail.com>
2022-09-12 18:00:02 -0300

    agragando promesas

---
+++ 02-Fundamentos/promesas.js
@@ -0,0 +1,64 @@
+const empleados=[
+    {
+        id:1,
+        nombre:'Fernando'
+    },
+    {
+        id:2,
+        nombre:'Linda'
+    },
+    {
+        id:3,
+        nombre:'Karen'
+    }
+];
+const salarios=[
+    {
+        id:1,
+        salario:1000
+    },
+    {
+        id:2,
+        salario:1500
+    }
+];
+
+const getEmpleado=(id)=>{
+
+    return new Promise((resolve,reject)=>{
+
+        const empleado=empleados.find(e=>e.id===id)?.nombre;
+
+    (empleado)
+        ?resolve(empleado)
+        :reject(`No existe Empleado con id ${id}`);
+    
+
+    });
+    
+}
+
+const getSalario=(id)=>{
+
+    return new Promise((resolve,reject)=>{
+
+        const salario=salarios.find(s=>s.id===id)?.salario;
+
+        (salario)
+            ?resolve(salario)
+            :reject(`Salario de empleado id ${id} no existe`);
+
+    });  
+    
+}
+
+const id=3;
+
+getEmpleado(id)
+    .then(empleado=>console.log(empleado))
+    .catch(err=>console.log(err))
+
+
+getSalario(id)
+    .then(salario=>console.log(salario))
+    .catch(err=>console.log(err))
